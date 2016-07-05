2eca2ff69bab4dab6ae87107f4cc7c98768e7da8
Emanuel Gordis <emanuelgordis@gmail.com>
2016-07-05 12:36:18 -0700

    no message

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] validCharacters = new string[] { "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O",
+            "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^",
+                "&", "*", "(", ")", "?", };
+            int passwordLength = 16;
+            int length = 0;
+            Random random = new Random();
+            string[] password = new string[100];
+            int arrayLength = validCharacters.Length;
+
+            while (length < passwordLength)
+            {
+                int randomInt = random.Next(arrayLength);
+                password[length] = validCharacters[randomInt];
+                length++;
+            }
+           string passwordAsString = string.Join("", password);
+            Console.WriteLine(passwordAsString);
+            Console.WriteLine(length);
+        }
+       
+    }
+}
